607ad19aa509bf6212aa6ef5c459a80a06cb7dda
red42 <mperepelitsa421@gmail.com>
2019-10-16 12:17:04 +0300

    Updates

---
+++ precache-manifest.be6d09e8d2e5033091e72d8e015cda44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99230118c7e8f49de16fc610cdf2f56",
+    "url": "/react-cat-app/index.html"
+  },
+  {
+    "revision": "77f9472e06ecfe9d8449",
+    "url": "/react-cat-app/static/css/main.b8544e39.chunk.css"
+  },
+  {
+    "revision": "ac48eddb1b1fe0e73876",
+    "url": "/react-cat-app/static/js/2.213c4605.chunk.js"
+  },
+  {
+    "revision": "77f9472e06ecfe9d8449",
+    "url": "/react-cat-app/static/js/main.3a994c83.chunk.js"
+  },
+  {
+    "revision": "f7bbe4229add54d161f4",
+    "url": "/react-cat-app/static/js/runtime-main.6fb65a70.js"
+  }
+]);
